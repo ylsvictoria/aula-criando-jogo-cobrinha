e8b17d4bbf1e28638046e04788a778d98b97b339
Victoria Souza <victoria.ls@hotmail.com>
2020-11-19 20:56:26 -0300

    definindo aleatoriedade da fruta e crescimento da cobra

--- script.js
+++ script.js
@@ -50,6 +50,7 @@ function iniciarJogo(){
 criarBG();
 criarCobrinha();
 drawFood();
+
 let snakeX = snake[0].x;
 let snakeY = snake[0].y;
 
@@ -58,7 +59,12 @@ if(direction == "left") snakeX -= box;
 if(direction == "up") snakeY -= box;
 if(direction == "down") snakeY += box;
 
-snake.pop();
+if(snakeX != food.x || snakeY != food.y){
+    snake.pop();
+}else{food.x =  Math.floor(Math.random() * 15 + 1) * box;
+     food.y = Math.floor(Math.random() * 15 + 1) * box;
+
+}
 
 let newHead = {
     x: snakeX,
