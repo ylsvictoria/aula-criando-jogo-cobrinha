f2968054ba59ba44fcd476bc35a06cc754e3107f
Victoria Souza <victoria.ls@hotmail.com>
2020-11-26 11:06:17 -0300

    mudando fonte e cor do background

--- script.js
+++ script.js
@@ -28,12 +28,14 @@ function criarCobrinha(){
     }
 }
 
+
+
+
 function drawFood(){
     context.fillStyle = "red";
     context.fillRect(food.x, food.y, box, box);
 }
 
-document.addEventListener('keydown', update);
 
 function update (event){
     if(event.keyCode == 37 && direction !="right") direction = "left";
@@ -52,7 +54,7 @@ function iniciarJogo(){
     for(i = 1; i < snake.length; i++){
         if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
             clearInterval(jogo);
-            alert("Game Over :(")
+            alert("Game Over =( ")
         }
     }
 
